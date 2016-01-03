c4c1b535ea352b068cab7b477962eb29233c1312
kureta <skureta@gmail.com>
2016-01-04 00:01:44 +0200

    added addon_config.cmake file for old ofx3DModelLoader

---
+++ openFrameworks/addons/ofx3DModelLoader/addon_config.cmake
@@ -0,0 +1,10 @@
+list(APPEND ADDON_SOURCE_FILES
+        ${OF_DIRECTORY}/addons/ofx3DModelLoader/src/ofx3dModelLoader.cpp
+        ${OF_DIRECTORY}/addons/ofx3DModelLoader/src/3DS/model3DS.cpp
+        ${OF_DIRECTORY}/addons/ofx3DModelLoader/src/3DS/texture3DS.cpp
+        )
+
+list(APPEND OF_ADDON_HEADERS
+        ${OF_DIRECTORY}/addons/ofx3DModelLoader/src/
+        ${OF_DIRECTORY}/addons/ofx3DModelLoader/3DS
+        )
